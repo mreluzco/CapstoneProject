43f92982630826cf05cd52f2b7855e56f932e324
mreluzco <83735435+mreluzco@users.noreply.github.com>
2024-05-13 09:14:28 -0400

    Update OpenWeatherAPI2.R

--- OpenWeatherAPI2.R
+++ OpenWeatherAPI2.R
@@ -29,16 +29,18 @@ season <- c()
 # Get forecast data for a given city list
 get_weather_forecaset_by_cities <- function(city_names){
   df <- data.frame()
-  for (city_name in city_names){
-    # Forecast API URL
-    forecast_url <- 'https://api.openweathermap.org/data/2.5/forecast'
-    # Create query parameters
-    forecast_query <- list(q = city_name, appid = "f18693430f0978f5e467497f60664bc4", units="metric")
-    # Make HTTP GET call for the given city
-    weather_response <- GET(forecast_url, query = forecast_query)
-    json_list <- content(weather_response, as="parsed")
-    # Note that the 5-day forecast JSON result is a list of lists. You can print the reponse to check the results
-    results <- json_list$list
+  city <- c(city, city_name)
+      weather <- c(weather, result$weather[[1]]$main)
+      visibility <- c(visibility, result$visibility)
+      temp <- c(temp, result$temp)
+      temp_min <- c(temp_min, result$main$temp_min)
+      temp_max <- c(temp_max, result$main$temp_max)
+      pressure <- c(pressure, result$main$pressure)
+      humidity <- c(humidity, result$main$humidity)
+      wind_speed <- c(wind_speed, result$wind$speed)
+      wind_deg <- c(wind_deg, result$wind$deg)
+      forecast_datetime <- c(forecast_datetime, result$dt_txt)
+      season <- c(season, "winter")
     # Loop the json result
     for(result in results) {
       city <- c(city, json_list$city_name)
