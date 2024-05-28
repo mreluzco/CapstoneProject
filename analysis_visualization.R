fc0cb788a7e2b6d32696641b2558a00b22b16a1c
mreluzco <83735435+mreluzco@users.noreply.github.com>
2024-05-28 13:25:44 -0400

    Update analysis_visualization.R

--- analysis_visualization.R
+++ analysis_visualization.R
@@ -5,8 +5,11 @@ library(ggplot2)
 seoul_bike_sharing <- "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-RP0321EN-SkillsNetwork/labs/datasets/seoul_bike_sharing.csv"  
 bike_share_df <- read_csv(seoul_bike_sharing)
 
-#make date column date class data
-bike_share_df$DATE <- as.Date(bike_share_df$DATE)
+#make date column date class data, and SEASONS and FUNCTIONING_DAY factors
+bike_share_df$DATE <- as.Date(bike_share_df$DATE, "%m/%d/%Y")
+bike_share_df$SEASONS <- as.factor(bike_share_df$SEASONS)
+bike_share_df$FUNCTIONING_DAY <- as.factor(bike_share_df$FUNCTIONING_DAY)
+bike_share_df
 
 #cast HOURS as a categorical variable and coerce levels to be sequenced
 bike_share_df$HOUR <- factor(bike_share_df$HOUR, ordered = TRUE)
@@ -44,8 +47,11 @@ summary <- grouped_df %>%
 print(summary)
 
 #create a scatter plot of RENTED_BIKE_COUNT vs DATE
-ggplot(bike_share_df,
-       aes(x = DATE, y = RENTED_BIKE_COUNT)) +
-      geom_point(alpha = 0.1) 
+ggplot(bike_share_df) +
+  geom_point(aes(x=DATE, y=RENTED_BIKE_COUNT, alpha = 0.3)) 
 
-\
+#create the same plot, but using hours as the color
+ggplot(bike_share_df) +
+  geom_point(aes(x=DATE, y=RENTED_BIKE_COUNT, color=HOUR, alpha = 0.3)) 
+
+
